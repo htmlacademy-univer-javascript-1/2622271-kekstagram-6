81391df58530ec9769a656bd28fdd952d976ffbe
Воронина Ксения <kseniavoronina1211@gmail.com>
2025-11-18 11:26:51 +0500

    8.11. Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -8,6 +8,10 @@ const commentCountBlock = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 
+let currentComments = [];
+let commentsShown = 0;
+const COMMENTS_PER_PORTION = 5;
+
 const createCommentElement = (comment) => {
   const commentElement = document.createElement('li');
   commentElement.classList.add('social__comment');
@@ -24,15 +28,31 @@ const createCommentElement = (comment) => {
   return commentElement;
 };
 
-const renderComments = (comments) => {
-  socialComments.innerHTML = '';
+const renderComments = () => {
+  const commentsToShow = currentComments.slice(commentsShown, commentsShown + COMMENTS_PER_PORTION);
 
   const fragment = document.createDocumentFragment();
-  comments.forEach(comment => {
+  commentsToShow.forEach((comment) => {
     fragment.appendChild(createCommentElement(comment));
   });
 
   socialComments.appendChild(fragment);
+
+  commentsShown += commentsToShow.length;
+
+  const commentCountText = `${commentsShown} из ${currentComments.length} комментариев`;
+  commentCountBlock.innerHTML = '';
+  commentCountBlock.textContent = commentCountText;
+
+  if (commentsShown >= currentComments.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+};
+
+const onCommentsLoaderClick = () => {
+  renderComments();
 };
 
 const openBigPicture = (pictureData) => {
@@ -42,21 +62,26 @@ const openBigPicture = (pictureData) => {
   commentsCount.textContent = pictureData.comments.length;
   socialCaption.textContent = pictureData.description;
 
-  renderComments(pictureData.comments);
+  currentComments = pictureData.comments;
+  commentsShown = 0;
+  socialComments.innerHTML = '';
 
-  commentCountBlock.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  renderComments();
+
+  commentCountBlock.classList.remove('hidden');
 
   bigPicture.classList.remove('hidden');
-
   document.body.classList.add('modal-open');
+
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
 };
 
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
-};
 
+  commentsLoader.removeEventListener('click', onCommentsLoaderClick);
+};
 
 closeButton.addEventListener('click', () => {
   closeBigPicture();
