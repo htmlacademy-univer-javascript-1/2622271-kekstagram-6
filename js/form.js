11b0aef957482baff32e38124d5d31b32a245aeb
Воронина Ксения <kseniavoronina1211@gmail.com>
2025-12-05 00:24:50 +0500

    9.13. Помощь друга

--- js/form.js
+++ js/form.js
@@ -1,14 +1,21 @@
+//для загрузки изображения
+
 import { isEscKey } from './util.js';
+import { initEffectsModule, resetEffectsModule } from './effects.js';
 
 const form = document.querySelector('.img-upload__form');
 const uploadInput = form.querySelector('#upload-file');
 const uploadOverlay = form.querySelector('.img-upload__overlay');
 const closeButton = form.querySelector('#upload-cancel');
+const hashtagsInput = form.querySelector('.text__hashtags');
+const commentInput = form.querySelector('.text__description');
 const body = document.body;
 
 const openUploadForm = () => {
   uploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
+
+  initEffectsModule();
 
   document.addEventListener('keydown', onDocumentKeydown);
   closeButton.addEventListener('click', onCloseButtonClick);
@@ -20,12 +27,21 @@ const closeUploadForm = () => {
 
   form.reset();
 
+  resetEffectsModule();
+
   document.removeEventListener('keydown', onDocumentKeydown);
   closeButton.removeEventListener('click', onCloseButtonClick);
 };
 
 const onDocumentKeydown = (evt) => {
   if (isEscKey(evt)) {
+    const isHashtagsFocused = document.activeElement === hashtagsInput;
+    const isCommentFocused = document.activeElement === commentInput;
+
+    if (isHashtagsFocused || isCommentFocused) {
+      return;
+    }
+
     evt.preventDefault();
     closeUploadForm();
   }
@@ -36,22 +52,24 @@ const onCloseButtonClick = (evt) => {
   closeUploadForm();
 };
 
-// Обработчик выбора файла
 const onFileInputChange = () => {
   if (uploadInput.files && uploadInput.files.length > 0) {
-    //подстановка изображения
+    //подстановка фото
+
     openUploadForm();
   }
 };
 
-// Инициализация формы
 const initUploadForm = () => {
   uploadInput.addEventListener('change', onFileInputChange);
 
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    //обработка отправки формы
-    console.log('Форма отправлена');
+
+    const formData = new FormData(form);
+
+    // отправка на сервер
+    // closeUploadForm();
   });
 };
 
